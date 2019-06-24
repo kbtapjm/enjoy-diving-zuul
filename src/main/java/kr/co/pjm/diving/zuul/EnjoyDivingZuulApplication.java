df46d10c9c944ec3e21b6392afdb99fd3c8b6cab
jmpark\pjm <kbtapjm@gmail.clm>
2019-06-25 08:28:13 +0900

    first commit

---
+++ src/main/java/kr/co/pjm/diving/zuul/EnjoyDivingZuulApplication.java
@@ -0,0 +1,13 @@
+package kr.co.pjm.diving.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnjoyDivingZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnjoyDivingZuulApplication.class, args);
+	}
+
+}
